aa04ddddb6d6e7208e8882c4d4999963ab47d9d6
blevm <mjblevins7@gmail.com>
2018-05-16 16:44:38 -0400

    mb solution

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@p_all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@p_all << self
+  end
+
+  def self.all
+    @@p_all
+  end
+
+  def self.clear
+    @@p_all.clear
+  end
+
 end
