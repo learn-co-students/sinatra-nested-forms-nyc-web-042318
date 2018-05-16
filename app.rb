52cfa4cb3e86834314d51a9ea5f75abc81e53de7
Derek Chen <derekgc@icloud.com>
2018-05-16 13:35:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,37 @@
 require './environment'
+require "pry"
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      ship = params["pirate"]["ships"][0]
+      pirate = params["pirate"]
+
+      @s1 = Ship.new(ship["name"],ship["type"],ship["booty"])
+
+      ship = params["pirate"]["ships"][1]
+      @s2 = Ship.new(ship["name"],ship["type"],ship["booty"])
+      # binding.pry
+
+      @p = Pirate.new(pirate["name"],pirate["weight"],pirate["height"])
+
+
+
+      erb :'pirates/show'
+    end
+
+
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  ALL = []
+
+  def initialize(name,type,booty)
+    @name = name
+    @type = type
+    @booty = booty
+    ALL << self
+  end
+
+
+  def self.all
+    ALL
+  end
+
+  def self.clear
+    while (ALL.length > 0)
+      ALL.pop
+    end
+    ALL
+  end
+
 end
