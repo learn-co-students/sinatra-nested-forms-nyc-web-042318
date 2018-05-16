beb669a443402c4f3f9a6bbcdad9df0a5f8f372b
ensallee <ensallee@gmail.com>
2018-05-16 18:54:37 -0400

    solved

--- app.rb
+++ app.rb
@@ -4,6 +4,34 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
+
+    get '/pirates' do
+      @pirates = Pirate.all
+
+      erb :'pirates/index'
+    end
+
+    get '/new' do
+
+      erb :'pirates/new'
+    end
+
+    get '/pirates/:id' do
+      erb :'pirates/show'
+    end
+
+    post '/pirates' do
+      @pirate=Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+      @ship_1=Ship.new(params["pirate"]["ships"][0]["name"], params["pirate"]["ships"][0]["type"], params["pirate"]["ships"][0]["booty"])
+      @ship_2=Ship.new(params["pirate"]["ships"][1]["name"], params["pirate"]["ships"][1]["type"], params["pirate"]["ships"][1]["booty"])
+
+
+      # redirect "/pirates/#{@pirate.id}"
+      erb :'pirates/show'
+    end
 
   end
 end
